6e9b41319fdfa31bb869d242c6856cdd7952fe53
wahyu <wahyu>
2022-05-14 18:22:16 +0700

    fixed global color setup

--- src/config/typhoGraphy.ts
+++ src/config/typhoGraphy.ts
@@ -4,39 +4,48 @@ import { StyleSheet } from "react-native";
 import { RFPercentage } from "../utils";
 
 import FONTS from "./fonts";
+import COLORS from "./colors";
 
 const typhoGraphy = StyleSheet.create({
   GothamLight: {
     fontFamily: FONTS.GothamLight,
     fontSize: RFPercentage(2.5),
+    color: COLORS.black.main,
   },
   GothamLightBold: {
     fontFamily: FONTS.GothamBold,
     fontSize: RFPercentage(2.5),
+    color: COLORS.black.main,
   },
   GothamRegular: {
     fontFamily: FONTS.GothamMedium,
     fontSize: RFPercentage(2.8),
+    color: COLORS.black.main,
   },
   GothamRegularBold: {
     fontFamily: FONTS.GothamBold,
     fontSize: RFPercentage(2.8),
+    color: COLORS.black.main,
   },
   GothamSemiLarge: {
     fontFamily: FONTS.GothamMedium,
     fontSize: RFPercentage(3.7),
+    color: COLORS.black.main,
   },
   GothamSemiLargeBold: {
     fontFamily: FONTS.GothamBold,
     fontSize: RFPercentage(3.7),
+    color: COLORS.black.main,
   },
   GothamLarge: {
     fontFamily: FONTS.GothamMedium,
     fontSize: RFPercentage(3.9),
+    color: COLORS.black.main,
   },
   GothamLargeBold: {
     fontFamily: FONTS.GothamBold,
     fontSize: RFPercentage(3.9),
+    color: COLORS.black.main,
   },
 });
 
